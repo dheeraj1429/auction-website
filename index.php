398dec9173f591453b8eb0dcaf4029ecc3a4c1f3
Uday lal <uday.lal@bonamisoftware.com>
2022-01-03 16:44:36 +0530

    Update index.php and generate.php

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
 <?php
+require "./model/general.php";
+
+function getGeneral()
+{
+    $general = new General();
+    return $general;
+}
+
+function getValuesByName($name)
+{
+    $general = getGeneral();
+    $data = $general->getDataByName($name)[0];
+    return $data["key_value"];
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=`device-width`, initial-scale=1.0">
+    <title>Smart Auction</title>
+</head>
+
+<body>
+    <h1>Home Page</h1>
+</body>
+
+</html>
