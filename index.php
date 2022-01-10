b0c5988ae1c818ecb6c0dabaa80d0b826c367167
Uday lal <uday.lal@bonamisoftware.com>
2022-01-10 15:51:45 +0530

    Update blog section of index.php

--- index.php
+++ index.php
@@ -1729,7 +1729,7 @@ $howItWorks = $cmsPages->getCMSByType(2);
                         <h1><?php echo $b["name"] ?></h1>
 
                         <p class="post_discription light_para">
-                            <?php echo htmlspecialchars_decode($b["desc"]) ?>
+                            <?php echo htmlspecialchars_decode($b["short_desc"]) ?>
                         </p>
                         <!-- <p class="post_discription light_para">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                             Nulla nobis at fuga dolore voluptates nisi ut. Autem commodi ullam, veniam provident
@@ -1738,84 +1738,15 @@ $howItWorks = $cmsPages->getCMSByType(2);
                             cum, iusto ipsum? Nisi veritatis necessitatibus ullam. Praesentium, ducimus. Ut ipsam quos
                             illum doloremque inventore ex.</p> -->
 
-                        <button class="Read_more_button my-3">Read More <img src="./assests/icons&images/Vector (2).svg"
-                                alt=""></button>
+                        <a href="./blog.php?id=<?php echo $b["id"] ?>" target="_blank" class=" Read_more_button
+                            my-3">Read More <img src="./assests/icons&images/Vector (2).svg" alt=""></a>
                         <!-- Post Details -->
                     </div>
                     <!-- Posts -->
                 </div>
                 <?php endforeach; ?>
-                <div class="col-12 col-sm-12 col-md-4 col-lg-4">
-                    <!-- Posts -->
-                    <div class="post_div">
-                        <!-- Post Previwe -->
-                        <div class="post_img_div">
-                            <img src="./assests/icons&images/Rectangle 19.png" alt="">
-                            <div class="post_date_div">
-                                <h3 class="text-white">13 <span>DEC</span></h3>
-                            </div>
-                        </div>
-                        <!-- Post Previwe -->
-                        <!-- Post Details -->
-                        <div class="my-3 d-flex align-items-center">
-                            <img src="./assests/icons&images/Group 4.svg" alt="">
-                            <p class="ms-3 light_para">By Giulia May</p>
-                        </div>
 
-                        <h1>Technology series-11 Controller</h1>
-
-                        <p class="post_discription light_para">Lorem, ipsum dolor sit amet consectetur adipisicing
-                            elit. Cum
-                            debitis vero
-                            quae sed, maiores numquam
-                            doloremque ipsam cupiditate maxime pariatur, voluptates ipsa iure illum veritatis. Quos
-                            voluptates error
-                            quod illo.</p>
-
-                        <button class="Read_more_button my-3">Read More <img src="./assests/icons&images/Vector (2).svg"
-                                alt=""></button>
-                        <!-- Post Details -->
-                    </div>
-                    <!-- Posts -->
-                </div>
-
-                <div class="col-12 col-sm-12 col-md-4 col-lg-4">
-                    <!-- Posts -->
-                    <div class="post_div">
-                        <!-- Post Previwe -->
-                        <div class="post_img_div">
-                            <img src="./assests/icons&images/Rectangle 19-1.png" alt="">
-                            <div class="post_date_div">
-                                <h3 class="text-white">13 <span>DEC</span></h3>
-                            </div>
-                        </div>
-                        <!-- Post Previwe -->
-                        <!-- Post Details -->
-                        <div class="my-3 d-flex align-items-center">
-                            <img src="./assests/icons&images/Group 4.svg" alt="">
-                            <p class="ms-3 light_para">By Giulia May</p>
-                        </div>
-
-                        <h1>Technology series-11 Controller</h1>
-
-                        <p class="post_discription light_para">Lorem, ipsum dolor sit amet consectetur adipisicing
-                            elit. Cum
-                            debitis vero
-                            quae sed, maiores numquam
-                            doloremque ipsam cupiditate maxime pariatur, voluptates ipsa iure illum veritatis. Quos
-                            voluptates error
-                            quod illo.</p>
-
-                        <button class="Read_more_button my-3">Read More <img src="./assests/icons&images/Vector (2).svg"
-                                alt=""></button>
-                        <!-- Post Details -->
-                    </div>
-                    <!-- Posts -->
-                </div>
             </div>
-            <!-- News Post Div -->
-
-        </div>
     </section>
     <!-- Latest News Update -->
 
