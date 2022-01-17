234c21eb7da12b87abf3ffcec6a080ad90ebfe3e
Uday lal <uday.lal@bonamisoftware.com>
2022-01-17 23:44:16 +0530

    Add rechet library for real time connection

--- test.php
+++ test.php
@@ -1,10 +1,27 @@
 <?php
-// $command = "crontab " . "/opt/lampp/htdocs/auction/cronJobs/cronFile.txt";
+?>
 
-// $out = shell_exec($command);
+<!DOCTYPE html>
+<html lang="en">
 
-// echo $out;
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-// echo "Done";
-$out = shell_exec("echo 'test'");
-echo $out;
+<body>
+    <script>
+    var conn = new WebSocket('ws://localhost:8080');
+    conn.onopen = function(e) {
+        console.log("Connection established!");
+    };
+
+    conn.onmessage = function(e) {
+        console.log(e.data);
+    };
+    </script>
+</body>
+
+</html>
