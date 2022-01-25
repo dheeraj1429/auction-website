542bea38832543f47866cceaaa9aafcaef170b5f
Uday lal <uday.lal@bonamisoftware.com>
2022-01-25 13:08:40 +0530

    remove unwanted codes from legal page

--- legal.php
+++ legal.php
@@ -28,25 +28,6 @@ $pageName = "legal";
         </div>
     </section>
     <!-- Content -->
-    <div class="c-alert danger">
-        <div class="icon">
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-check-all"
-                viewBox="0 0 16 16">
-                <path
-                    d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992a.252.252 0 0 1 .02-.022zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486-.943 1.179z" />
-            </svg>
-        </div>
-        <div class="message">test</div>
-        <div class="close">
-            <button id="close">
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x"
-                    viewBox="0 0 16 16">
-                    <path
-                        d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                </svg>
-            </button>
-        </div>
-    </div>
 </main>
 
 <!-- Footer -->
